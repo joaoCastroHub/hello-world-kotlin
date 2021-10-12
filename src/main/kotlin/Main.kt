0ff26bf8abb5c26a011fe64622778f143007129f
joaoCastroHub <joao_lukas97@hotmail.com>
2021-10-11 21:54:30 -0300

    EXERCICIO

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,10 +7,6 @@ fun main(args: Array<String>) {
     feedTheFish()
 }
 
-fun fitMoreFish(tankSize: Int, currentFish: Array<Int>, fishSize: Int, hasDecorations: Boolean) {
-
-}
-
 fun feedTheFish() {
     val day = randomDay()
     val food = fishFood(day)
@@ -41,5 +37,25 @@ fun fishFood(day: String): String {
 fun randomDay(): String {
     val week = listOf("Sunday", "Monday", "Thuesday", "Wednesday", "Thursday", "Friday", "Saturday")
     return week[Random().nextInt(7)]
+}
+
+//Função de fit more fish
+
+fun fitMoreFish(tankSize: Double, currentFish: List<Int>, fishSize: Int = 2, hasDecorations: Boolean = true) :Boolean {
+    var ocupado = 0;
+    for(i in currentFish) {
+        ocupado += currentFish[i-1]
+    }
+
+    if(hasDecorations) {
+       var space = tankSize.times(0.8)
+        if(space <= ocupado) return false
+        if(space < (ocupado + fishSize)) return false
+        return true
+    } else {
+        if(tankSize <= ocupado) return false
+        if(tankSize < (ocupado + fishSize)) return false
+        return true
+    }
 }
 
