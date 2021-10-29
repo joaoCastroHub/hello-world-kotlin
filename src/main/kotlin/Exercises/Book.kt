f49f47cb91448dcf6a104e4e46ae1f0c680856d9
joaoCastroHub <joao_lukas97@hotmail.com>
2021-10-29 19:17:53 -0300

    exercicio

--- src/main/kotlin/Exercises/Book.kt
+++ src/main/kotlin/Exercises/Book.kt
@@ -1,12 +1,31 @@
 package Exercises
 
 open class Book(var title: String = "", var author: String = "") {
+    const val MAX_NUMBER_BOOKS = 20
     private var currentPage: Int = 1
 
     open fun readPage() {
         currentPage += 1
     }
+
+    fun canBorrow(hasBooks: Int):Boolean {
+        return (hasBooks <= MAX_NUMBER_BOOKS)
+    }
+
+    fun printUrl() {
+        println(Constants.BASE_URL + title + ".html")
+    }
+
+    companion object {
+        val BASE_URL = "http://www.turtlecare.net/"
+    }
+
+    object Constants {
+        const val BASE_URL = "http://www.turtlecare.net/"
+    }
 }
+
+
 
 class eBook(title: String, author: String, var format: String = "text") : Book(title, author) {
     private var wordsRead = 0
